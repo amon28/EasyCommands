45ceefc3dfc919e4adf78ec3cfce2838a85e6275
amon28 <richmondgerez@gmail.com>
2021-08-13 16:17:14 +0800

    Update EasyCommands.js

--- scripts/EasyCommands.js
+++ scripts/EasyCommands.js
@@ -5,15 +5,16 @@ import * as Minecraft from 'Minecraft';
 export class EasyCommands{
 	
 	//accepts entity obj and entity type
+	//returns 
 	static getTags(entity,entityType){
-	let EntityName = entity.nameTag;
-	try{
-	let list = 	Minecraft.Commands.run(`tag @e[name="${EntityName}",type=${entityType}] list`).statusMessage;		
-		let str = list.split(`:`);
-		let tagList = str[1].split(`,`);
-		return tagList;
-	}catch(e){	
-	Minecraft.Commands.run(`say ${e}`);
+		let EntityName = entity.nameTag;
+		try{
+		let list = 	Minecraft.Commands.run(`tag @e[name="${EntityName}",type=${entityType}] list`).statusMessage;		
+			let str = list.split(`:`);
+			let tagList = str[1].split(`,`);
+				return tagList;
+		}catch(e){	
+		Minecraft.Commands.run(`say ${e}`);
 		}
 	}
 }
