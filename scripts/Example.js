f4befcb85962cdb8e304157f289cf943675667e8
amon28 <richmondgerez@gmail.com>
2021-08-13 19:59:14 +0800

    Update Example.js

--- scripts/Example.js
+++ scripts/Example.js
@@ -21,7 +21,7 @@ function beforeChat(entityData)
 		case "!taglist":
 		let tags = EasyCommands.getTags(`tag @e[name=${entityData.sender.name},type=player] list`);
 			for(var c in tags){
-				str = str + "," + tags[c];
+				str = str + tags[c] + ",";
 			}			
 				str = "Tags:" + str;
 			Minecraft.Commands.run(`tellraw ${entityData.sender.name} {"rawtext":[{"text":"${str}"}]}`);
@@ -29,18 +29,50 @@ function beforeChat(entityData)
 		break;
 		
 		case "!victim":
-		let victims = EasyCommands.getVictims(`tag @e[name=${entityData.sender.name},type=player] list`);
+		let victims = EasyCommands.getVictims(`testfor @e[type=player]`);
 			for(var c in victims){
-			str = str + "," + victims[c];
+			str = str + victims[c] + ",";
 			}
 			str = "Victims:" + str;
 			Minecraft.Commands.run(`tellraw ${entityData.sender.name} {"rawtext":[{"text":"${str}"}]}`);
-			//output: all of the names of the player gotten by testfor
+			//output: all of the names of the player affected by testfor
+		break;
+		
+		case "!playercount":
+		let count = "Count: " + EasyCommands.playerCount();
+			Minecraft.Commands.run(`tellraw ${entityData.sender.name} {"rawtext":[{"text":"${count}"}]}`);
+			//output: current player count
+		break;
+		
+		case "!maxplayers":
+		let max = "Max Count: " + EasyCommands.playerCountMax();
+			Minecraft.Commands.run(`tellraw ${entityData.sender.name} {"rawtext":[{"text":"${max}"}]}`);
+			//output: max player count the world can hold
+		break;
+		
+		case "!getplayers":
+		let players = EasyCommands.getAllPlayers();
+			for(var c in players){
+				str = str + players[c] + ",";
+			}
+			str = "players: " + str;
+			Minecraft.Commands.run(`tellraw ${entityData.sender.name} {"rawtext":[{"text":"${str}"}]}`);
+			//output: get all player names in the world
+		break;
+		
+		case "!clearedplayers":
+		let cplayers = EasyCommands.getClearedPlayers(`clear @a`);
+			for(var c in cplayers){
+				str = str + cplayers[c] + ",";
+			}
+			str = "players: " + str;
+			Minecraft.Commands.run(`tellraw ${entityData.sender.name} {"rawtext":[{"text":"${str}"}]}`);
+			//output: get player names whos items got cleared
 		break;
 		
 		//this is for testing
 		case "!test":
-		let test = Minecraft.Commands.run(`execute @a[c=1] ~ ~ ~ testforblock ~ ~-1 ~ grass 0`);
+		let test = Minecraft.Commands.run(`clear @a apple 0 1`);
 			for(var c in test){
 			Minecraft.Commands.run(`say ${c} - ${test[c]}`);
 			}
